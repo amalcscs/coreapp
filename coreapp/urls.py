c22f99210a193f154d1066fee85423a915b0c30a
amal c s <amalcs0006@gmail.com>
2022-02-05 17:13:19 +0530

    seven

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -2,10 +2,26 @@ from django.urls import re_path, include
 from . import views
 
 urlpatterns=[
+    # amal
     re_path(r'^$', views.index, name='index'),
     re_path(r'^tldashboard$', views.tldashboard, name='tldashboard'),
     re_path(r'^tlprojects$', views.tlprojects, name='tlprojects'),
     re_path(r'^tlprojecttasks$', views.tlprojecttasks, name='tlprojecttasks'),
     re_path(r'^tlsplittask$', views.tlsplittask, name='tlsplittask'),
     re_path(r'^tlgivetask$', views.tlgivetask, name='tlgivetask'),
+# abin
+    re_path(r'^TLattendance$', views.TLattendance, name='TLattendance'),
+    re_path(r'^TLreportissues$', views.TLreportissues, name='TLreportissues'),
+    re_path(r'^TLreportedissue1$', views.TLreportedissue1, name='TLreportedissue1'),
+    re_path(r'^TLreportedissue2$', views.TLreportedissue2, name='TLreportedissue2'),
+    re_path(r'^TLreport1$', views.TLreport1, name='TLreport1'),
+    re_path(r'^TLreportsuccess$', views.TLreportsuccess, name='TLreportsuccess'),
+    # bibn
+    re_path(r'^TLtasks$', views.TLtasks, name='TLtasks'),
+    re_path(r'^TLleave$', views.TLleave, name='TLleave'),
+    re_path(r'^TLleavereq$', views.TLleavereq, name='TLleavereq'),
+    re_path(r'^TLreqedleave$', views.TLreqedleave, name='TLreqedleave'),
+    re_path(r'^TLgivetask$', views.TLgivetask, name='TLgivetask'),
+    re_path(r'^TLgavetask$', views.TLgavetask, name='TLgavetask'),
+    re_path(r'^TLsuccess$', views.TLsuccess, name='TLsuccess'),
 ]
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
-
+# amal
 def index(request):
     return render(request, 'TLindex.html')
 
@@ -19,3 +19,43 @@ def tlsplittask(request):
 
 def tlgivetask(request):
     return render(request, 'TLgivetask.html')
+# abin
+def TLattendance(request):
+    return render(request, 'TLattendance.html')
+
+def TLreportissues(request):
+    return render(request, 'TLreportissues.html')
+
+def TLreportedissue1(request):
+    return render(request, 'TLreportedissue1.html')
+
+def TLreportedissue2(request):
+    return render(request, 'TLreportedissue2.html')
+
+def TLreport1(request):
+    return render(request, 'TLreport1.html')
+
+def TLreportsuccess(request):
+    return render(request, 'TLreportsuccess.html')
+
+# bibin
+def TLtasks(request):
+    return render(request, 'TLtasks.html')
+
+def TLleave(request):
+    return render(request, 'TLleave.html')
+
+def TLleavereq(request):
+    return render(request, 'TLleavereq.html')
+
+def TLreqedleave(request):
+    return render(request, 'TLreqedleave.html')
+
+def TLgivetask(request):
+    return render(request, 'TLgivetasks.html')
+
+def TLgavetask(request):
+    return render(request, 'TLgavetask.html')
+    
+def TLsuccess(request):
+    return render(request, 'TLsuccess.html')
